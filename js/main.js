7d1c6c37332f6ee0d36700da96e7de7253982a4b
Cho <j_cho6@tss-hr420-dm37.fc.ca>
2018-11-09 11:20:47 -0500

    class updated

--- js/main.js
+++ js/main.js
@@ -1,33 +1,55 @@
 (() => {
+    //rework with a Vue instance
+
+    const vm = new Vue({
+
+        el : "#app",
+        data : {
+            modelname : "",
+            modelpricing : "",
+            modeldetails : ""
+        },
+        methods : {
+            fetchData(e) {
+             //   debugger;
+                let targetURL = e.currentTarget.id; // gets the id of the element via the event object
+        
+
+                fetch(`./includes/connect.php?carModel=${targetURL}`) // go get data and bring it back! good doogy
+        
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const { modelName, pricing, modelDetails } = data[0];
+
+                    this.modelname = modelName;
+                    this.modeldetails = modelDetails;
+                    this.modelpricing = pricing;
+                    // parseCarData(data[0]);
+                })
+                .catch(function(error) {
+                    console.error(error);
+                });
+            }
+        }
+
+    })
 
     //grab the car button
-    const cars = document.querySelectorAll('.data-ref');
-    function fetchData() {
-        let targetURL = `./includes/connect.php?carModel=${this.id}`;
+    // const cars = document.querySelectorAll('.data-ref');
 
-        fetch(targetURL) // go get data and bring it back! good doogy
 
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            parseCarData(data[0]);
-        })
-        .catch(function(error) {
-            console.error(error);
-        });
-    }
+    // function parseCarData(car) {
+    //     debugger;
+    //     // take the database data and put it on the page
+    //     const { modelName, pricing, modelDetails } = car;
+    //     // take the database data and put it on the page
+    //     document.querySelector(".modelName").textContent = modelName;
+    //     document.querySelector(".priceInfo").textContent = pricing;
+    //     document.querySelector(".modelDetails").textContent = modelDetails;
+    // };
 
-    function parseCarData(car) {
-        debugger;
-        // take the database data and put it on the page
-        const { modelName, pricing, modelDetails } = car;
-        // take the database data and put it on the page
-        document.querySelector(".modelName").textContent = modelName;
-        document.querySelector(".priceInfo").textContent = pricing;
-        document.querySelector(".modelDetails").textContent = modelDetails;
-    };
+    // cars.forEach(car => car.addEventListener("click", fetchData));
 
-    cars.forEach(car => car.addEventListener("click", fetchData));
-
-    //fetchData();
+    // //fetchData();
 })();
