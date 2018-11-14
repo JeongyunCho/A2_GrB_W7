9318d8a5768ec9f6eb1134dbf33e8ff7f03a0a03
Cho <j_cho6@tss-hr420-dm34.fc.ca>
2018-11-14 10:30:06 -0500

    mount,update

--- js/main.js
+++ js/main.js
@@ -1,33 +1,83 @@
 (() => {
 
-    //grab the car button
-    const cars = document.querySelectorAll('.data-ref');
-    function fetchData() {
-        let targetURL = `./includes/connect.php?carModel=${this.id}`;
-
-        fetch(targetURL) // go get data and bring it back! good doogy
-
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            parseCarData(data[0]);
-        })
-        .catch(function(error) {
-            console.error(error);
-        });
-    }
-
-    function parseCarData(car) {
-        debugger;
-        // take the database data and put it on the page
-        const { modelName, pricing, modelDetails } = car;
-        // take the database data and put it on the page
-        document.querySelector(".modelName").textContent = modelName;
-        document.querySelector(".priceInfo").textContent = pricing;
-        document.querySelector(".modelDetails").textContent = modelDetails;
-    };
-
-    cars.forEach(car => car.addEventListener("click", fetchData));
-
-    //fetchData();
-})();
+    // VARIABLES
+  
+    const vm = new Vue({
+      el: '#app',
+  
+      data: {
+        carModel: '',
+        carDescription: '',
+        carPricing: ''
+      },
+  
+      mounted : function() {
+        //listen for when Vue is done building itself
+        console.log('mounted');
+  
+        //get the data for the first car
+        document.querySelector("#F55").click();
+      },
+  
+      updated : function(){
+        //listen for when vue completes its update / render cycle
+        console.log('updated');
+      },
+  
+      methods: {
+  
+        getData(e) {
+  
+          let targetURL = `./includes/connect.php?carModel=${e.currentTarget.id}`;
+  
+          fetch(targetURL) // go get the data and bring it back
+            .then(res => res.json()) // turn the result into a plain JS object
+            .then(data => {
+              console.log(data);
+              //run a function to parse our data
+              this.showCarData(data[0]); // run a function to put the data on the page
+            }) // let's see what we got
+            .catch(function (error) {
+              console.log(error); // if anything broke, log it to the console
+            });
+        },
+  
+        showCarData(data) {
+          this.carModel = data.model;
+          this.carDescription = data.modelDetails;
+          this.carPricing = data.pricing;
+          console.log(data)
+        }
+  
+      }
+    });
+  
+    // FUNCTIONS
+    // function getData() {
+    //   let targetURL = "./includes/connect.php?modelNo=R58";
+  
+    //   fetch(targetURL) // go get the data and bring it back
+    //     .then(res => res.json()) // turn the result into a plain JS object
+    //     .then(data => {
+    //       console.log(data);
+    //       //run a function to parse our data
+    //       showCarData(data[0]); // run a function to put the data on the page
+    //     }) // let's see what we got
+    //     .catch(function (error) {
+    //       console.log(error); // if anything broke, log it to the console
+    //     });
+    // }
+    // function showCarData(data) {
+    //   //debugger;
+    //   //parse the db info and put it where it needs to go
+    //   const { modelName, pricing, modelDetails } = data; // destructuring assignment => MDN JS destructuring
+  
+    //   document.querySelector(".modelName").textContent = modelName;
+    //   document.querySelector(".priceInfo").textContent = `$ ${pricing}.00`;
+    //   document.querySelector(".modelDetails").textContent = modelDetails;
+    // }
+  
+    // EVENTS
+    //getData(); // trigger the getData function
+  
+  })();
